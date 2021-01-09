c29a74c723940cb91085337ec255cd1c95868044
Nikolaj Johannes Skole Jensen <10490273+nikolajjsj@users.noreply.github.com>
2021-01-09 16:12:24 +0100

    Add custom card widget

--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class CardWidget extends StatelessWidget {
   final String title;
   final Color color;
+  final Function onTap;
 
   const CardWidget({
     Key key,
     @required this.title,
     @required this.color,
+    @required this.onTap,
   }) : super(key: key);
 
   @override
@@ -16,16 +18,19 @@ class CardWidget extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-      child: Material(
-        color: color,
-        elevation: 1.0,
-        borderRadius: BorderRadius.circular(12.0),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12.0),
-          child: Center(
-            child: Text(
-              title,
-              style: _textTheme.headline4.apply(fontWeightDelta: 4),
+      child: InkWell(
+        onTap: onTap,
+        child: Material(
+          color: color,
+          elevation: 2.0,
+          borderRadius: BorderRadius.circular(12.0),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: Center(
+              child: Text(
+                title,
+                style: _textTheme.headline4.apply(fontWeightDelta: 4),
+              ),
             ),
           ),
         ),
