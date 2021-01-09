8ad3fd447f4294e0d44dd2a20226aee4b9ff2969
Nikolaj Johannes Skole Jensen <10490273+nikolajjsj@users.noreply.github.com>
2021-01-09 16:13:22 +0100

    Changed game model

--- lib/models/game_model.dart
+++ lib/models/game_model.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:scoreboard/models/player_model.dart';
 
 class Game {
   final String title;
   final int maxScore;
   final int roundsCount;
   final bool reversedScoring;
+  final List<Player> players;
 
   const Game({
     @required this.title,
     this.maxScore,
     this.roundsCount,
     @required this.reversedScoring,
+    @required this.players,
   });
+
+  Game.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        maxScore = json['maxScore'],
+        roundsCount = json['roundsCount'],
+        reversedScoring = json['reversedScoring'],
+        players =
+            json['players'].map((player) => Player.fromJson(player)).tolist();
+
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'maxScore': maxScore,
+        'roundsCount': roundsCount,
+        'reversedScoring': reversedScoring,
+        'players': players.map((player) => player.toJson()).toList(),
+      };
 }
