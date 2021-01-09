30529f168f462c45ea09efe00ce1068e8afd5adb
Nikolaj Johannes Skole Jensen <10490273+nikolajjsj@users.noreply.github.com>
2021-01-09 11:24:32 +0100

    added Settings screen

---
+++ lib/screens/settings/settings_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  static Route route() {
+    return CupertinoPageRoute(builder: (context) => SettingsScreen());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+    );
+  }
+}
